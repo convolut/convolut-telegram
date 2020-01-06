4a32484496aff293aca47b3454fa929fe3dc77a7
Andrey Avdeev <seorazer@gmail.com>
2020-01-06 03:45:32 +0300

    version 0.0.2 was published with dependecy fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 with io.open("README.md", "r", encoding="utf-8") as f:
     readme = f.read()
 
-version = "0.0.1"
+version = "0.0.2"
 
 setup(
     name="convolut_telegram",
@@ -21,7 +21,7 @@ setup(
     zip_safe=False,
     python_requires=">=3.7",
     install_requires=[
-        "convolut>=0.0.3"
+        "convolut>=0.0.4"
     ],
     keywords="convolut telegram",
     url="https://github.com/convolut/convolut-telegram",
